fbfa7e2067c0e46838590cd3146265e422473892
bellyrose831 <145377757+bellyrose831@users.noreply.github.com>
2025-02-08 23:42:58 -0600

    Update script.js
    added spinning flowers

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+gsap.registerPlugin(ScrollTrigger, MotionPathPlugin)
+
 document.addEventListener('DOMContentLoaded',
     function () {
 
@@ -15,100 +17,111 @@ document.addEventListener('DOMContentLoaded',
             //ease: "steps(30)",
             ease: "slow(0.7,0.7,false)",
             fontSize: "30px"}, "<");
+
+        //make family things visible
+        tl.to(".card", {
+            opacity: 1,
+            duration: 3
+        }, "+=1")
+
+        tl.to(".flower", {
+            opacity: 1,
+            duration: 3
+        }, "+=2")
         
         tl.to(".cardtext", {
             color: "#F72585",
-            duration: 1}, ">");
+            duration: 1,
+            repeat: -1,
+            repeatDelay: 7
+        }, ">");
 
         
         tl.to(".cardtext", {
             color: "#4CC9F0",
-            duration: 1});
+            duration: 1,
+            repeat: -1,
+            repeatDelay: 7
+        });
 
 });
 
-/*var codingCard = document.getElementById('coding');
-codingCard.addEventListener('mouseover', cardHover('coding'));
 
-var readingCard = document.getElementById('reading');
-readingCard.addEventListener('mouseover', cardHover('reading'));
-
-var gamesCard = document.getElementById('games');
-gamesCard.addEventListener('mouseover', cardHover('games')); */
-
-let hoverAnimationOther = gsap.timeline()
-
-hoverAnimationOther.to("#other", {
-    paused: true,
-    height: "100%",
-    width: "100%",
-    duration: 4,
-    rotation: 720
+let flowerSpin = gsap.timeline({ paused: true });
+flowerSpin.to("#petal1", {
+    rotate: "+=360deg",
+    transformOrigin: "100% 100%",
+    duration: 3,
+    repeat: -1,
+    ease: "linear"
 });
-hoverAnimationOther.to(".flexbox", {
-    paused: true,
-    height: "100%",
-    width: "100%",
-    duration: 4
+flowerSpin.to("#petal2", {
+    rotate: "+=360deg",
+    transformOrigin: "100% 0%",
+    duration: 3,
+    repeat: -1,
+    ease: "linear"
 }, "<");
+flowerSpin.to("#petal3", {
+    rotate: "+=360deg",
+    transformOrigin: "0% 0%",
+    duration: 3,
+    repeat: -1,
+    ease: "linear"
+}, "<"); 
+flowerSpin.to("#petal4", {
+    rotate: "+=360deg",
+    transformOrigin: "0% 100%",
+    duration: 3,
+    repeat: -1,
+    ease: "linear"
+}, "<"); 
 
-function middleFunction () {
-    gsap.to("#other", {
-        paused: true,
-        height: "100%",
-        width: "100%",
-        duration: 4,
-        rotation: 720
-    });
-    gsap.to(".flexbox", {
-        paused: true,
-        height: "100%",
-        width: "100%",
-        duration: 4
-    });
-}
-
-
-let whatever = (
-    gsap.to(".flexbox", {
-        paused: true,
-        position: "absolute",
-        x: 0,
-        y: 0,
-        height: "100%",
-        width: "100%",
-        duration: 4
-    }),
-    gsap.to("#other", {
-        paused: true,
-        height: "100%",
-        width: "100%",
-        duration: 4,
-        rotation: 720
-    })
-);
+var flower1 = document.getElementById("petal1");
+flower1.addEventListener("mouseenter", () => flowerSpin.progress(0).play());
+var flower2 = document.getElementById("petal2");
+flower2.addEventListener("mouseenter", () => flowerSpin.progress(0).play());
+var flower3 = document.getElementById("petal3");
+flower3.addEventListener("mouseenter", () => flowerSpin.progress(0).play());
+var flower4 = document.getElementById("petal4");
+flower4.addEventListener("mouseenter", () => flowerSpin.progress(0).play());
 
 
-var otherCard = document.getElementById('other');
-otherCard.addEventListener("mouseenter", () => whatever.progress(0).play());
-//otherCard.addEventListener("mouseenter", () => hoverAnimationOther.progress(0).play(););
+let flowerSpin2 = gsap.timeline({ paused: true });
+flowerSpin2.to("#petal5", {
+    rotate: "-=360deg",
+    transformOrigin: "100% 100%",
+    duration: 2,
+    repeat: -1,
+    ease: "linear"
+});
+flowerSpin2.to("#petal6", {
+    rotate: "-=360deg",
+    transformOrigin: "100% 0%",
+    duration: 2,
+    repeat: -1,
+    ease: "linear"
+}, "<");
+flowerSpin2.to("#petal7", {
+    rotate: "-=360deg",
+    transformOrigin: "0% 0%",
+    duration: 2,
+    repeat: -1,
+    ease: "linear"
+}, "<"); 
+flowerSpin2.to("#petal8", {
+    rotate: "-=360deg",
+    transformOrigin: "0% 100%",
+    duration: 2,
+    repeat: -1,
+    ease: "linear"
+}, "<"); 
 
-/*
-otherCard.addEventListener('mouseover', middleFunction);
-
-function middleFunction () {
-    cardHover('other');
-}
-
-function cardHover (cardid) {
-    cardid = String('#' + cardid);
-    let hovertimeline = gsap.timeline();
-    hovertimeline.to(cardid, {
-        height: "100%",
-        width: "100%",
-        duration: 4,
-        rotation: 720,
-        oncomplete: () => hovertimeline.pause()
-    });
-    hovertimeline.play();
-} */
+var flower5 = document.getElementById("petal5");
+flower5.addEventListener("mouseenter", () => flowerSpin2.progress(0).play());
+var flower6 = document.getElementById("petal6");
+flower6.addEventListener("mouseenter", () => flowerSpin2.progress(0).play());
+var flower7 = document.getElementById("petal7");
+flower7.addEventListener("mouseenter", () => flowerSpin2.progress(0).play());
+var flower8 = document.getElementById("petal8");
+flower8.addEventListener("mouseenter", () => flowerSpin2.progress(0).play());
